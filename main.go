7106ca0bb26f914c48acbc7d0354f14fbc166d6b
Yash <ypt7895@gmail.com>
2024-12-07 20:40:10 +0530

    reading from .env

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	loadEnv()
+	serveApplication()
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func serveApplication() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
